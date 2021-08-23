dde7251ee91e379bdaea2434d74ca6d88237dd73
alenkruth <alenkruth@gmail.com>
2021-08-23 15:46:46 +0530

    replace constrained gen with loops

--- modules/decoder/decoder_i_ext_r_type.py
+++ modules/decoder/decoder_i_ext_r_type.py
@@ -3,9 +3,17 @@ from ruamel.yaml import YAML
 import uarch_test.regex_formats as rf
 import re
 import os
-from uarch_test.uarch_modules.modules.decoder.i_ext_r_type import gen
 
+#TO-DO inst_list and reg_vals will come from instrcutions constants file
+global inst_list, reg_vals
 
+inst_list = [
+    'add', 'and', 'or', 'sll', 'slt', 'sltu', 'sra', 'srl', 'sub', 'xor',
+    'addw', 'sllw', 'sraw', 'srlw', 'subw'
+    ]
+
+reg_vals = ['x' + str(s) for s in range(32)]
+ 
 class decoder_i_ext_r_type(IPlugin):
 
     def __init__(self):
@@ -16,19 +24,14 @@ class decoder_i_ext_r_type(IPlugin):
 
     def generate_asm(self):
         """
-          Generates the ASM file containing R type instructions present in the I extension
-        """
-        try:
-            asm = gen()
-        except Exception as e:
-            print(
-                'Could not resolve constraints using current seed. Trying again'
-            )
-            try:
-                asm = gen()
-            except Exception as e:
-                print("Exception Occured again. Test not created")
-                pass
+          Generates the ASM file containing R type instructions present in the I extension"""        
+        asm = ''
+        for inst in inst_list:
+            for reg1 in reg_vals:
+                for reg2 in reg_vals:
+                    for reg3 in reg_vals:
+                        asm = asm + f'{inst} {reg1}, {reg2}, {reg3}\n'
+        
         return (asm)
 
     def check_log(self, log_file_path, reports_dir):
