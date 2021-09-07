7f9e5b6d5f8783b36a3f98270f6e1d963f3393b9
purushothaman-p <purushothsankari@yahoo.com>
2021-09-07 16:23:55 +0530

    documented utg_decoder_i_ext_r_type.py

--- modules/decoder/utg_decoder_i_ext_r_type.py
+++ modules/decoder/utg_decoder_i_ext_r_type.py
@@ -1,9 +1,12 @@
 from yapsy.IPlugin import IPlugin
 from utg.instruction_constants import base_reg_file, arithmetic_instructions
-from random import randint
 
 
 class utg_decoder_arith_tests(IPlugin):
+    """
+    This class contains methods to generate and validate the tests for
+    arithmetic instructions.
+    """
 
     def __init__(self) -> None:
         pass
@@ -13,14 +16,30 @@ class utg_decoder_arith_tests(IPlugin):
 
     def generate_asm(self) -> str:
         """
-            Generates the ASM file containing R type instructions present in the I extension"
+            Generates the ASM instructions for R type arithmetic instructions.
+
+            Presently it creates asm for
+                add rd, rs1, rs2
+                sub rd, rs1, rs2
+                sll rd, rs1, rs2
+                sllw rd, rs1, rs2
+                sra rd, rs1, rs2
+                sraw rd, rs1, rs2
+                srl rd, rs1, rs2
+                srlw rd, rs1, rs2
         """
+        # rd, rs1, rs2 iterate through all the 32 register combinations for
+        # every instruction in arithmetic_instructions['add-sub-reg'] => add/sub
+
         asm = '#'*5 + ' add/sub reg, reg, reg ' + '#'*5 + '\n'
-        for inst in arithmetic_instructions['add-sub-reg']:
-            for rd in base_reg_file:
-                for rs1 in base_reg_file:
-                    for rs2 in base_reg_file:
+        for rd in base_reg_file:
+            for rs1 in base_reg_file:
+                for rs2 in base_reg_file:
+                    for inst in arithmetic_instructions['add-sub-reg']:
                         asm += f'{inst} {rd}, {rs1}, {rs2}\n'
+
+        # Again rd, rs1, rs2 iterate through the 32 register combinations for
+        # every instruction in arithmetic_instructions['shift-rl-reg']
         asm = '\n' * 2 + '#' * 5 + ' shift_inst reg, reg, reg ' + '#' * 5 + '\n'
         for inst in arithmetic_instructions['shift-rl-reg']:
             for rd in base_reg_file:
