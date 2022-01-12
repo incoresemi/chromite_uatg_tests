17cd237bda669b7a00a3ec4420059e691f489def
Karthik B K <bkkarthik01@gmail.com>
2022-01-12 17:01:28 +0530

    Update alignments

--- modules/caches/uatg_icache_set_thrashing_02.py
+++ modules/caches/uatg_icache_set_thrashing_02.py
@@ -6,6 +6,7 @@ from ruamel.yaml import YAML
 import uatg.regex_formats as rf
 from typing import Dict, Union, Any, List
 import random
+import math
 
 class uatg_icache_set_thrashing_02(IPlugin):
     def __init__(self):
@@ -29,15 +30,14 @@ class uatg_icache_set_thrashing_02(IPlugin):
         """Generate asm as a list of strings that have different
         alignments based on which set they're looking to thrash."""
         iter = self._word_size * self._block_size
-        ins_list = [f"\tli t1, {iter}\n" + \
-            f"".join(
-                [f"\t.align {self._word_size}\n"
-                for j in range(self._block_size)]
-            ) + "ins_j:\n" + \
+        ins_list = [f"\tli t1, {iter}\n\t.align " \
+            f"{int(math.log2(self._sets))}\n" \
+            "ins_j:\n" + \
             f"".join(
                 [str(elem) for elem in [
-                    f"\t.align {self._word_size}\n"
-                    for k in range(self._block_size)]
+                    "\tnop\n\t.align " \
+                    f"{int(math.log2(self._word_size * self._block_size))}\n"
+                    for k in range(i)]
                 ]) + \
             f"\taddi t1, t1, -1\n\tbeqz t1, end\n\tj ins_j\nend:\n\tnop"
             for i in range(self._sets)]
