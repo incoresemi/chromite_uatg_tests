b3399535093e7d222d3a11067bbc06b65f5b78d1
alenkruth <alenkruth@gmail.com>
2022-01-17 15:14:08 +0530

    update to execute test in supervisor mode

--- modules/branch_predictor/uatg_gshare_fa_ghr_ones_01.py
+++ modules/branch_predictor/uatg_gshare_fa_ghr_ones_01.py
@@ -51,19 +51,29 @@ class uatg_gshare_fa_ghr_ones_01(IPlugin):
         asm = f"\n\taddi t0, x0, {loop_count}\n\taddi t1,x0 ,0 \n\nloop:\n"
         asm += "\taddi t1, t1, 1\n\tblt t1, t0, loop\n"
 
+        # trap signature bytes
+        trap_sigbytes = 24
+        trap_count = 0
+        
+        # initialize the signature region
+        sig_code = 'mtrap_count:\n'
+        sig_code += ' .fill 1, 8, 0x0\n'
+        sig_code += 'mtrap_sigptr:\n'
+        sig_code += ' .fill {0},4,0xdeadbeef\n'.format(
+                    int(trap_sigbytes / 4))
         # compile macros for the test
-        compile_macros = []
+        compile_macros = ['rvtest_mtrap_routine']
 
         supervisor_dict = {
                 'enable': True,
                 'page_size': 4096,
-                'paging_mode': 'sv57',
+                'paging_mode': 'sv39',
                 'll_pages': 64,
                 'u_bit': False}
 
         return [{
             'asm_code': asm,
-            'asm_sig': '',
+            'asm_sig': sig_code,
             'compile_macros': compile_macros,
             'supervisor_mode': supervisor_dict,
             'docstring': 'This test fills ghr register with ones'
