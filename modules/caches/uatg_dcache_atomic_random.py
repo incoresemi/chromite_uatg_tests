48e874b66c05b07a17f4f33da6d011b65762553b
Karthik B K <bkkarthik01@gmail.com>
2022-01-19 15:17:21 +0530

    changed variables names

--- modules/caches/uatg_dcache_atomic_random.py
+++ modules/caches/uatg_dcache_atomic_random.py
@@ -2,7 +2,7 @@
 
 from yapsy.IPlugin import IPlugin
 from typing import Dict, Union, Any, List
-from uatg.instruction_constants import atomic_mem_ops as test
+from uatg.instruction_constants import atomic_mem_ops as inst_dict
 import random
 
 
@@ -36,7 +36,7 @@ class uatg_dcache_atomic_random(IPlugin):
             # We generate random 8 byte numbers.
             asm_data += f"\t.dword 0x{random.randrange(16 ** 16):8x}\n"
         
-        tests = test['rv64-mem-ops']
+        inst_list = inst_dict['rv64-mem-ops']
 
         asm_main = "\tfence\n\tli t0, 69\n\tli t1, 1"
         asm_main += f"\n\tli t3, {self._sets * self._ways}"
@@ -46,18 +46,18 @@ class uatg_dcache_atomic_random(IPlugin):
         asm_lab1 += "beq t4, t3, end\n\taddi t4, t4, 1\n\tj lab1\n"
         asm_lab1 += "end:\n\tla t2, rvtest_data\n\t"
         for i in range(500):
-            tempt = random.choice(tests)
-            tempe = list(tempt[-1])
-            x = random.randint(0, 2000)
-            y = 0
-            if 'w' in tempe:
-                y = 4
-            elif 'd' in tempe:
-                y = 8
-            asm_lab1 += 'li t1, ' + str(y * x) + '\n\t'
+            inst = random.choice(inst_list)
+            extension = list(inst[-1])
+            offset = random.randint(0, 2000)
+            boundary = 0
+            if 'w' in extension:
+                boundary = 4
+            elif 'd' in extension:
+                boundary = 8
+            asm_lab1 += 'li t1, ' + str(boundary * offset) + '\n\t'
             asm_lab1 += 'li t4, ' + str(random.randint(0, 2000)) + '\n\t'
             asm_lab1 += "add t3, t2, t1\n\t"
-            asm_lab1 += tempt + " t0, t4, 0(t3)\n\n\t"
+            asm_lab1 += inst + " t0, t4, 0(t3)\n\n\t"
 
         asm = asm_main + asm_lab1
         compile_macros = []
